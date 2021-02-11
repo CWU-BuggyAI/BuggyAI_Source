4dfb5a7eda339062e90e6d7265721a469fe24c65
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2021-02-10 19:48:13 -0800

    Server Side Code
    Adding all server side code used for the Flask REST API.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BuggyAI_Source
-This
+This is the main repository for the BuggyAI Code. We have split code for this repository by where it is used in a production environment.
+
+Under the Server_Code directory is a Flask REST API which resides on an Ubuntu 18.04 server using Nginx as a Reverse Proxy in front of Gunicorn which runs the Flask app.
+
+Under the (insert directory here) directory is the actual training and testing code for our model.
+
+
+--- Credits To ---
+
+[Xiaoping Wu and Team] -- For granting permission to use the IP102 dataset. Our project does not use any pretrained models or other source code, as we have developed and trained our own for our own purposes. We have only used the image dataset in training and validating our own model.
+
+@inproceedings{Wu2019Insect,
+  title={IP102: A Large-Scale Benchmark Dataset for Insect Pest Recognition},
+  author={Xiaoping Wu and Chi Zhan and Yukun Lai and Ming-Ming Cheng and Jufeng Yang},
+  booktitle={IEEE CVPR},
+  pages={8787--8796},
+  year={2019},
+}
---
+++ Server_Code/buggy.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing import image
+from PIL import Image
+import numpy as np
+import pathlib
+
+app = Flask(__name__)
+CORS(app)
+
+# Loading the ML model
+print("Loading ML Model")
+model = load_model('models/buggyAI_alpha.h5')
+
+@app.route('/prediction', methods=["POST"])
+def pred():
+    print('[----- Buggy AI (POST) -----]')
+
+    imgFile = request.files['file']
+    img = Image.open(imgFile)
+    img = np.asarray(img.resize((180, 180)))
+
+    img_array = image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    class_names = ['aphids', 'red-spider-mites', 'thrips']
+
+    pred = model.predict(img_array)
+    score = tf.nn.softmax(pred[0])
+
+    prediction = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
+    return jsonify(prediction)
+
+@app.route('/', methods=["GET"])
+def home():
+    print('[----- Buggy AI (GET) -----]')
+
+    resp = ('Welcome to the BuggyAI API, this is a test of the GET request. '
+    'Our application mainly handles POST requests. In order to run a image classification '
+    'with our model you must pass a .png or .jpg image in the form of a POST request.')
+
+    print(resp)
+    return render_template('index.html')
