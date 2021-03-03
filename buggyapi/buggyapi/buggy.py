dc9e789aea74958bf6964921cbf2fe1b08357d79
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2021-03-02 19:34:17 -0800

    Update buggy.py

--- buggyapi/buggyapi/buggy.py
+++ buggyapi/buggyapi/buggy.py
@@ -12,7 +12,7 @@ CORS(app)
 
 # Loading the ML model
 print("Loading ML Model")
-model = load_model('models/buggyAI_alpha.h5')
+model = load_model('models/buggyAI_V4.1.h5')
 
 @app.route('/prediction', methods=["POST"])
 def pred():
@@ -25,7 +25,7 @@ def pred():
     img_array = image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-    class_names = ['aphids', 'red-spider-mites', 'thrips']
+    class_names = ['Aphids', 'Cicadellidae', 'Lycorma_Delicatula', 'Miridae', 'None']
 
     pred = model.predict(img_array)
     score = tf.nn.softmax(pred[0])
@@ -35,24 +35,12 @@ def pred():
 
 @app.route('/', methods=["GET"])
 def home():
-    print('[----- Buggy AI (GET) -----]')
-
-    resp = ('Welcome to the BuggyAI API, this is a test of the GET request. '
-    'Our application mainly handles POST requests. In order to run a image classification '
-    'with our model you must pass a .png or .jpg image in the form of a POST request.')
-
-    print(resp)
     return render_template('index.html')
 
 @app.route('/About.html', methods=["GET"])
 def about():
-    print('[----- Buggy AI (GET) -----]')
-
     return render_template('About.html')
-
 
 @app.route('/Manual.html', methods=["GET"])
 def manual():
-    print('[----- Buggy AI (GET) -----]')
-
     return render_template('Manual.html')
